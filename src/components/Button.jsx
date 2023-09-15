6938ff49b696053bb2642c498c602cd6a2bd4625
Walshwade-dev <mainace8@gmail.com>
2023-09-15 20:59:27 +0300

    first commit

---
+++ src/components/Button.jsx
@@ -0,0 +1,10 @@
+
+import styles from '../style'
+
+const Button = () => {
+  return (
+    <button type="button" className={`py-4 px-6 bg-blue-gradient font-poppins font-medium text-[18px] mt-2 rounded-[10px] outline-none ${styles}`}>Get Started</button>
+  )
+}
+
+export default Button
